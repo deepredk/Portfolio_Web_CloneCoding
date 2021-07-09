beb6d9dc431ee5a7b5e62ee722150a03b5ad33fb
Deepred <deepred.kim@gmail.com>
2021-07-09 11:45:46 +0900

    JS까지 Ellie 코드로 바꿔치기

--- main.js
+++ main.js
@@ -3,9 +3,8 @@
 // Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
-
 document.addEventListener('scroll', () => {
-  if (window.scrollY >= navbarHeight) {
+  if (window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');
   } else {
     navbar.classList.remove('navbar--dark');
@@ -14,14 +13,12 @@ document.addEventListener('scroll', () => {
 
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
-navbarMenu.addEventListener('click', (e) => {
-  console.log(e.target.dataset.link);
-  const target = e.target;
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
   const link = target.dataset.link;
   if (link == null) {
     return;
   }
-
   navbarMenu.classList.remove('open');
   scrollIntoView(link);
 });
@@ -33,8 +30,8 @@ navbarToggleBtn.addEventListener('click', () => {
 });
 
 // Handle click on "contact me" button on home
-const contactBtn = document.querySelector('.home__contact');
-contactBtn.addEventListener('click', () => {
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
   scrollIntoView('#contact');
 });
 
@@ -64,25 +61,23 @@ arrowUp.addEventListener('click', () => {
 const workBtnContainer = document.querySelector('.work__categories');
 const projectContainer = document.querySelector('.work__projects');
 const projects = document.querySelectorAll('.project');
-
 workBtnContainer.addEventListener('click', (e) => {
-  const target =
-    e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
-
-  const filter = target.dataset.filter;
-
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
   if (filter == null) {
     return;
   }
 
-  // Remove selection from the previous titem and selec the new one
+  // Remove selection from the previous item and select the new one
   const active = document.querySelector('.category__btn.selected');
-  active.classList.remove('selected');
-  target.classList.add('selected');
+  if (active != null) {
+    active.classList.remove('selected');
+  }
+  e.target.classList.add('selected');
 
   projectContainer.classList.add('anim-out');
   setTimeout(() => {
     projects.forEach((project) => {
+      console.log(project.dataset.type);
       if (filter === '*' || filter === project.dataset.type) {
         project.classList.remove('invisible');
       } else {
@@ -95,5 +90,5 @@ workBtnContainer.addEventListener('click', (e) => {
 
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({ behavior: 'smooth' })
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
 }
